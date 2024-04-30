6cc6434992a4d07f742b5732b58433fa948b78f7
rkdtmdgh <rkdtmdgh3@naver.com>
2024-04-30 18:59:52 +0900

    ksh

--- routes/memberRouter.js
+++ routes/memberRouter.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const memberService = require('../lib/service/memberService');
 const { printLog } = require('../lib/utils/logger');
 const uploads = require('../lib/utils/uploads');
+const jwt = require("jsonwebtoken");
 const singleUploadMiddleware = uploads.profileUpload.single('m_profile_thumbnail');
 
 let pp = require('../lib/passport/passport.js');
@@ -21,6 +22,14 @@ router.post('/sign_up_confirm', singleUploadMiddleware, (req, res) => {
     console.log('req.file====', req.file);
 
     memberService.sign_up_confirm(req, res);
+
+})
+
+// 로그인 확인
+router.post('/sign_in_confirm', (req, res) => {
+    printLog(DEFAULT_NAME, '/sign_in_confirm');
+
+    memberService.sign_in_confirm(req, res);
 
 })
 
@@ -50,9 +59,9 @@ router.get('/get_member', (req, res) => {
 
 // 입력 회원 정보 가져오기
 router.get('/get_search_member', 
-    passport.authenticate('jwt', { session: false }), 
+    // passport.authenticate('jwt', { session: false }), 
     (req, res) => {
-    printLog(DEFAULT_NAME, '/get_member');
+    printLog(DEFAULT_NAME, '/get_search_member');
 
     memberService.get_search_member(req, res);
 
